06babad9068629e5a95282a48e2bc851fca437a3
ChefDeProjet <abdelkader.moussa@outlook.fr>
2018-04-05 08:30:56 +0200

    Auto

--- src/test/MainCLient.java
+++ src/test/MainCLient.java
@@ -2,7 +2,13 @@ package test;
 
 import client.Client;
 
-public class MainCLient{
+public class MainCLient implements Runnable{
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	public static void main(String[] args) {
 		Client c = new Client(1234);
--- src/test/MainServeur.java
+++ src/test/MainServeur.java
@@ -2,14 +2,14 @@ package test;
 
 import serveur.Serveur;
 
-public class MainServeur{
-	/*@Override
+public class MainServeur implements Runnable {
+	
+	@Override
 	public void run() {
 		Serveur serveur = new Serveur(1234);
-		serveur.lancement();	
-	}*/
-	
-	
+		serveur.lancement();
+	}
+
 	public static void main(String[] args) {
 		Serveur serveur = new Serveur(1234);
 		serveur.lancement();
