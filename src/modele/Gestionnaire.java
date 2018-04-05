4ba17036860927fd742f61fa9e7ef0482be1356c
ChefDeProjet <abdelkader.moussa@outlook.fr>
2018-04-05 12:05:05 +0200

    correction rmi

---
+++ src/modele/Gestionnaire.java
@@ -0,0 +1,7 @@
+package modele;
+
+import java.util.ArrayList;
+
+public class Gestionnaire {
+	public static ArrayList<Personne> ToutesLesPersonnes = new ArrayList<Personne>();
+}
--- src/modele/Personne.java
+++ src/modele/Personne.java
@@ -44,6 +44,47 @@ public class Personne {
 		return "Personne [pseudo=" + pseudo + ", description=" + description + ", nbCommentaire=" + nbCommentaire
 				+ ", dateInscription=" + dateInscription + ", langues=" + langues + "]";
 	}
-	
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setNbCommentaire(int nbCommentaire) {
+		this.nbCommentaire = nbCommentaire;
+	}
+
+	public void setDateInscription(Date dateInscription) {
+		this.dateInscription = dateInscription;
+	}
+
+	public void setLangues(ArrayList<Langue> langues) {
+		this.langues = langues;
+	}
+
+	public void set(String attr, Object obj) {
+		switch (attr) {
+		case "pseudo":
+			this.setPseudo((String) obj);
+			break;
+		case "description":
+			this.setDescription((String) obj);
+			break;
+		case "nbCommentaire":
+			this.setNbCommentaire((int) obj);
+			break;
+		case "dateInscription":
+			this.setDateInscription(((Date) obj));
+			break;
+		case "langues":
+			this.setLangues((ArrayList<Langue>) obj);
+			break;
+		default:
+			break;
+		}
+	}
 
 }
